7a0d59ad83687cb50719b1b08fd7117324bda7d9
soeguet <osman_s@live.de>
2023-12-31 23:48:15 +0100

    almost finished, needs tests and recaching status updates

--- backend/index.mjs
+++ backend/index.mjs
@@ -8,6 +8,9 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./myDB.db");
 const app = express();
 const port = 3000;
+let recacheRunning = false;
+let recachingCurrent = 0;
+let recachingTotal = 0;
 
 // middleware setup
 app.use(cors());
@@ -25,28 +28,35 @@ async function fetchAllPdfFromDir(dir) {
     return fs.readdirSync(dir);
 }
 
+// TODO check if folder is empty
+
 /**
  *
  * Caches all PDFs in selected Folder.
  * @param {Array} pdfList
  */
-function cacheAllPdfsInDir(pdfList) {
+async function cacheAllPdfsInDir(pdfList) {
     console.log("start caching all PDFs");
 
-    // const pdfListLength = pdfList.length;
     for (const pdf of pdfList) {
-        let dataBuffer = fs.readFileSync(mainDir + pdf);
-        PDF(dataBuffer)
-            .then(async (bufferedPdf) => {
-                // if not in database, write to database
-                const pdfEntry = {
-                    name: pdf,
-                    pages: bufferedPdf.numpages,
-                    text: bufferedPdf.text,
-                };
-                await writePdfDataToDatabase(pdfEntry);
-            })
-            .catch((err) => console.log("Invalid PDF func" + err));
+        try {
+            let dataBuffer = fs.readFileSync(mainDir + pdf);
+            let bufferedPdf = await PDF(dataBuffer);
+
+            if (recacheRunning) {
+                recachingCurrent++;
+            }
+
+            const pdfEntry = {
+                name: pdf,
+                pages: bufferedPdf.numpages,
+                text: bufferedPdf.text,
+            };
+
+            await writePdfDataToDatabase(pdfEntry);
+        } catch (err) {
+            console.log("Error processing PDF " + pdf + ": " + err);
+        }
     }
 }
 
@@ -86,7 +96,7 @@ async function writePdfDataToDatabase(pdfDict) {
             }
         );
     });
-    console.log(`wrote ${pdfDict.name} to database`);
+    // console.log(`wrote ${pdfDict.name} to database`);
 }
 
 // query search terms from frontend
@@ -104,17 +114,74 @@ app.post("/api/v1/search", (req, res) => {
     query += queryParts.join(" AND ");
 
     // fire query
-    db.all(query, queryParams, (err, rows) => {
+    db.all(query, queryParams, async (err, rows) => {
         if (err) {
             console.error(err);
             res.status(500).send("internal server error");
             return;
         }
 
+        console.log(rows);
+
         if (rows.length > 0) {
             res.json(rows);
         } else {
-            res.status(404).send("no entries found");
+            res.json({ status: "no results", total: rows.length });
+        }
+    });
+});
+
+async function dropDatabaseTable(db) {
+    db.serialize(() => {
+        db.run("DROP TABLE IF EXISTS pdfs;");
+    });
+}
+
+app.get("/api/v1/folder-path", (_, res) => {
+    res.json({ path: mainDir });
+});
+
+app.post("/api/v1/recache", (req, res) => {
+    console.log("recaching started");
+    dropDatabaseTable(db)
+        .then(() => createDatabaseTable(db))
+        .then(() =>
+            fetchAllPdfFromDir(mainDir)
+                .then((pdfList) =>
+                    pdfList.filter((pdf) => pdf.endsWith(".pdf"))
+                )
+                .then(async (pdfList) => cacheAllPdfsInDir(pdfList))
+                .then(() => {
+                    recachingTotal = 0;
+                    recachingCurrent = 0;
+                    recacheRunning = false;
+                })
+                .catch((err) => console.log(err.message))
+        );
+});
+
+app.get("/api/v1/recache", (req, res) => {
+    if (recacheRunning) {
+        console.log("recaching running");
+        res.json({
+            status: "running",
+            current: recachingCurrent,
+            total: recachingTotal,
+        });
+    } else {
+        res.json({ status: "done" });
+    }
+});
+
+app.post("/api/v1/folder-path", (req, res) => {
+    console.log(req.body);
+    fs.access(req.body.path, fs.constants.R_OK, (err) => {
+        if (err) {
+            console.log("path not found");
+            res.status(404).send({ error: "Path not found" });
+        } else {
+            mainDir = req.body.path;
+            res.json({ path: mainDir, status: "ok" });
         }
     });
 });
@@ -122,8 +189,6 @@ app.post("/api/v1/search", (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
-
-await createDatabaseTable(db);
 
 async function getDbRowSize() {
     return new Promise((resolve, reject) => {
@@ -135,13 +200,16 @@ async function getDbRowSize() {
         });
     });
 }
-fetchAllPdfFromDir(mainDir)
+
+await createDatabaseTable(db)
+    .then(async () => await fetchAllPdfFromDir(mainDir))
     .then((pdfList) => pdfList.filter((pdf) => pdf.endsWith(".pdf")))
     .then(async (pdfList) => {
         const dbRowSize = await getDbRowSize();
         if (dbRowSize === pdfList.length) {
             throw new Error("No new PDFs to cache");
         }
+        return pdfList;
     })
     .then(async (pdfList) => cacheAllPdfsInDir(pdfList))
     .catch((err) => console.log(err.message));
--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,6 +1,123 @@
 document.addEventListener("DOMContentLoaded", () => {
     const searchBox = document.getElementById("searchBox");
     const resultsDiv = document.getElementById("results");
+    const statusFieldDiv = document.getElementById("status-field");
+    const pathField = document.getElementById("pathInputGrid");
+    const newPathField = document.getElementById("new-path");
+    const savePathButton = document.getElementById("send-new-path-button");
+    const recacheSpinner = document.getElementById("recache");
+
+    recacheSpinner.addEventListener("click", () => {
+        fetch("http://localhost:3000/api/v1/recache", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw response;
+                }
+            })
+            .catch((error) => {
+                if (error.status === 404) {
+                    statusFieldDiv.classList.add("text-danger");
+                    statusFieldDiv.innerHTML =
+                        "recaching failed" + error.message;
+                } else {
+                    console.error("Error:", error);
+                }
+            });
+
+        setTimeout(checkForRecacheStatus, 3000);
+    });
+
+    /**
+     * check if recaching is done - recursive
+     */
+    async function checkForRecacheStatus() {
+        console.log("checking for recache status");
+
+        try {
+            console.log("checking for recache status");
+            let response = await fetch("http://localhost:3000/api/v1/recache");
+            console.log("checking for recache status");
+
+            // Überprüfen Sie, ob der Fetch-Aufruf erfolgreich war
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            let jsonStatus = await response.json();
+
+            console.log(jsonStatus);
+
+            if (jsonStatus.status === "running") {
+                statusFieldDiv.classList.remove("text-warning");
+                statusFieldDiv.innerHTML =
+                    "recaching.. (" +
+                    jsonStatus.current +
+                    "/" +
+                    jsonStatus.total +
+                    ")";
+                setTimeout(checkForRecacheStatus, 3000);
+            } else {
+                console.log("recaching done");
+                statusFieldDiv.classList.add("text-success");
+                statusFieldDiv.innerHTML = "recaching done";
+            }
+        } catch (error) {
+            console.log("Error fetching recache status:", error);
+            // Behandlung von Netzwerkfehlern oder anderen Fehlern
+        }
+    }
+
+    savePathButton.addEventListener("click", () => {
+        const newPath = newPathField.value;
+        fetch("http://localhost:3000/api/v1/folder-path", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ path: newPath }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw response;
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // update path field with new path
+                pathField.value = data.path;
+            })
+            .catch((error) => {
+                if (error.status === 404) {
+                    statusFieldDiv.classList.add("text-danger");
+                    statusFieldDiv.innerHTML =
+                        "path not found, ignored request";
+                    // reset path field in modal back to old value
+                    newPathField.value = pathField.value;
+                } else {
+                    console.error("Error:", error);
+                }
+            });
+    });
+
+    function retrievePathToPdfs() {
+        new Promise((_, reject) => {
+            fetch("http://localhost:3000/api/v1/folder-path")
+                .then((response) => response.json())
+                .then((data) => {
+                    pathField.value = data.path;
+                    newPathField.value = data.path;
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    }
+    retrievePathToPdfs();
 
     searchBox.addEventListener(
         "input",
@@ -27,6 +144,19 @@ document.addEventListener("DOMContentLoaded", () => {
             })
                 .then((response) => response.json())
                 .then((data) => {
+                    if (
+                        data.status != undefined &&
+                        data.status === "no results"
+                    ) {
+                        statusFieldDiv.innerHTML =
+                            data.total +
+                            " results. you should recache your database.";
+                        throw new Error("no results");
+                    }
+
+                    return data;
+                })
+                .then((data) => {
                     displayResults(data);
 
                     // stop timer
@@ -36,8 +166,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
                     const timerField = document.getElementById("timer");
                     timerField.textContent = `${duration} ms`;
+                    statusFieldDiv.classList.remove("text-danger");
+                    statusFieldDiv.innerHTML = `${data.length} results`;
                 })
-                .catch((error) => console.error("Error:", error));
+                .catch((error) => {
+                    if (error.message === "no results") {
+                        statusFieldDiv.innerHTML =
+                            "no results. db size: " + statusFieldDiv.innerHTML;
+                    } else {
+                        statusFieldDiv.innerHTML = "error";
+                    }
+
+                    statusFieldDiv.classList.add("text-danger");
+                    console.error("Error:", error);
+                });
         }
     }
 
@@ -100,7 +242,7 @@ document.addEventListener("DOMContentLoaded", () => {
  */
 function debounceBackendRequest(func, delay) {
     let timeoutId;
-    return function () {
+    return function() {
         clearTimeout(timeoutId);
         timeoutId = setTimeout(() => {
             func.apply(this, arguments);

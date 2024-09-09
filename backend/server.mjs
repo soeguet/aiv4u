ce43083f1e8ecf9dbe8d6b7444a23c1f4c508751
soeguet <osman_s@live.de>
2024-09-09 20:28:05 +0200

    WIP: refactor server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -6,44 +6,85 @@ import {
 } from "./database.mjs";
 import { loadUserPath } from "./user-path.mjs";
 import { app } from "./app.mjs";
-import {cacheAllPdfsInDir, fetchAllPdfFromDir} from "./pdf.mjs";
-import path from "node:path";
+import { cacheAllPdfsInDir, fetchAllPdfFromDir } from "./pdf.mjs";
 import { homedir } from "node:os";
+import path from "node:path";
+import exec from "node:child_process";
 
+/** @type {number} */
 const port = 3000;
 
 /**
- * Wrapper for the main initialization.
+ * Makes the PDFs available to the frontend.
+ *
+ * @returns {Promise<string>} mainDir
  */
-async function main() {
-    createDatabaseTable(db);
-
+async function makePdfsAvailableToFrontend() {
     const configFile = path.join(homedir(), ".aiv4u.json");
     const mainDir = await loadUserPath(configFile);
 
     // make pdfs available to the frontend
     app.use("/pdf", express.static(mainDir));
+    return mainDir;
+}
 
+
+/**
+ * Retrieves all PDFs from the directory.
+ *
+ * @param {string} mainDir
+ *
+ * @returns {string[]}
+ */
+function retrievePdfsFromDir(mainDir) {
     const pdfList = fetchAllPdfFromDir(mainDir);
     pdfList.filter((pdf) => pdf.endsWith(".pdf"));
+    return pdfList;
+}
 
-    // 4) print out the number of pdfs in directory and in database
-    const dbRowSize = await getDbRowSize(db);
-    await consoleLogStatementsForTheTerminal(mainDir, dbRowSize, pdfList);
+/**
+ * Opens the URL in the default browser on Linux.
+ *
+ * @param {string} url
+ * @returns void
+ */
+function openUrlInDefaultBrowserOnLinux(url) {
 
-    // bail out if all pdfs are already in the database
-    if (dbRowSize === pdfList.length) {
-        return;
-    }
+    exec.exec(`xdg-open ${url}`, (error, _, stderr) => {
 
-    // otherwise cache PDFs
-    await cacheAllPdfsInDir(db, pdfList, writePdfDataToDatabase);
+        if (error) {
+            console.error(`Error opening URL: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`Error: ${stderr}`);
+            return;
+        }
+    });
 }
 
 // start server, cache pdfs and open server port
 app.listen(port, async () => {
-    await main();
-    console.log(`fuzzy finder app listening on port ${port}`);
+
+    createDatabaseTable(db);
+
+    const mainDir = await makePdfsAvailableToFrontend();
+    const pdfList = retrievePdfsFromDir(mainDir);
+    const dbRowSize = await getDbRowSize(db);
+
+    await consoleLogStatementsForTheTerminal(mainDir, dbRowSize, pdfList);
+
+    if (dbRowSize !== pdfList.length) {
+        await cacheAllPdfsInDir(db, pdfList, writePdfDataToDatabase);
+    }
+
+    const url = `http://localhost:${port}`;
+    console.log(`fuzzy finder app listening on ${url}`);
+
+    if (process.platform === "linux") {
+        openUrlInDefaultBrowserOnLinux(url);
+    }
 });
 
 /**
@@ -54,31 +95,18 @@ app.listen(port, async () => {
  */
 async function consoleLogStatementsForTheTerminal(mainDir, dbRowSize, pdfList) {
     console.log(`
-____________
-____________
 
-            ██████  ██████  ███████     ███████ ██    ██ ███████ ███████ ██    ██     ███████ ██ ███    ██ ██████  ███████ ██████  
-            ██   ██ ██   ██ ██          ██      ██    ██    ███     ███   ██  ██      ██      ██ ████   ██ ██   ██ ██      ██   ██ 
-            ██████  ██   ██ █████       █████   ██    ██   ███     ███     ████       █████   ██ ██ ██  ██ ██   ██ █████   ██████  
-            ██      ██   ██ ██          ██      ██    ██  ███     ███       ██        ██      ██ ██  ██ ██ ██   ██ ██      ██   ██ 
-            ██      ██████  ██          ██       ██████  ███████ ███████    ██        ██      ██ ██   ████ ██████  ███████ ██   ██ 
+ ██████  ██████  ███████     ███████ ██    ██ ███████ ███████ ██    ██     ███████ ██ ███    ██ ██████  ███████ ██████  
+ ██   ██ ██   ██ ██          ██      ██    ██    ███     ███   ██  ██      ██      ██ ████   ██ ██   ██ ██      ██   ██ 
+ ██████  ██   ██ █████       █████   ██    ██   ███     ███     ████       █████   ██ ██ ██  ██ ██   ██ █████   ██████  
+ ██      ██   ██ ██          ██      ██    ██  ███     ███       ██        ██      ██ ██  ██ ██ ██   ██ ██      ██   ██ 
+ ██      ██████  ██          ██       ██████  ███████ ███████    ██        ██      ██ ██   ████ ██████  ███████ ██   ██ 
             
-____________
-____________
-started process
+################# ################# ################# #################
+|| Chosen PDF Path: ${mainDir}
+|| ${dbRowSize} DB rows are present
+|| ${pdfList.length} PDFs are available
+|| = ${Math.floor((dbRowSize / pdfList.length) * 100)}% pdfs are in the database
+################# ################# ################# #################
 `);
-    console.log("____________");
-    console.log("____________");
-    console.log("");
-    console.log("dbRowSize: " + dbRowSize);
-    console.log("pdf path: " + mainDir);
-    console.log("pdfList.length: " + pdfList.length);
-    console.log(
-        `= ${(dbRowSize / pdfList.length) * 100}% pdfs are in the database`
-    );
-    console.log("");
-    console.log("____________");
-    console.log("____________");
-    console.log("");
-    console.log("");
 }
